4a2dc496f7dc7dc333345ede3f443c7b76185781
raunakbhattacharjee68-tech <raunakbhattacharjee68@gmail.com>
2026-06-01 02:50:35 +0530

    Solved minor bugs

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import router from express.Router();
+const router = express.Router();
 
 import { registerUser } from "../controllers/registerController.js";
 import { loginUser } from "../controllers/loginController.js";
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import router from express.Router();
+const router = express.Router();
 
 import { createUser, getUsers,getUserById,updateUser, deleteUser } from "../controllers/userController.js";
 
